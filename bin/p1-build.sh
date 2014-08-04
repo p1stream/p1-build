b533926d8944f71de72aabf70975f3df49510d2d
Stéphan Kochen <stephan@kochen.nl>
2014-08-04 19:43:59 +0200

    Move all vars to index.js and source.

--- bin/p1-build.sh
+++ bin/p1-build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
+# Setup environment.
+$(node -e 'require("p1-build").shell()')
+
 # Swap home directories for npm and node-gyp.
 export REALHOME="${HOME}"
 export HOME="${REALHOME}/.p1stream/.node"
-
-# Current atom-shell version.
-export atom_shell_version="0.15.2"
-
-# Current atom-shell node-gyp settings.
-export npm_config_dist_url="https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell/dist"
-export npm_config_target="0.11.13"
-export npm_config_arch="x64"
 
 # Execute command.
 exec $@
--- index.js
+++ index.js
@@ -1,2 +1,36 @@
+var os = require('os');
 var path = require('path');
+var util = require('util');
+
 exports.includeDir = path.join(__dirname, 'include');
+
+exports.platform = os.platform();
+exports.arch = 'x64';
+
+exports.nodeVersion = '0.11.13';
+exports.atomShellVersion = '0.15.2';
+
+exports.atomShellPackageUrl = util.format(
+    'https://github.com/atom/atom-shell/releases/download/v%s/atom-shell-v%s-%s-%s.zip',
+    exports.atomShellVersion, exports.atomShellVersion, exports.platform, exports.arch);
+
+exports.atomDistUrl = 'https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell/dist';
+
+exports.shell = function() {
+    var vars = {
+        p1stream_include_dir: exports.includeDir,
+        node_platform: exports.platform,
+        node_arch: exports.arch,
+        node_version: exports.version,
+        atom_shell_version: exports.atomShellVersion,
+        atom_shell_package_url: exports.atomShellPackageUrl,
+        atom_dist_url: exports.atomDistUrl,
+        npm_config_dist_url: exports.atomDistUrl,
+        npm_config_target: exports.nodeVersion,
+        npm_config_arch: exports.arch
+    };
+
+    Object.keys(vars).forEach(function(k) {
+        console.log('export %s=%s', k, vars[k]);
+    });
+};
